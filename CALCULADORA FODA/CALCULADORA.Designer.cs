fb727d701cbe2cae13a09bef27b7aa8bed3492b8
VictorCyuvi20 <victoramancio2020@gmail.com>
2024-10-01 10:47:08 -0300

    TELA CALCULADORA (CONFIGURAÇÕES SE ENCONTRAM AQUI)

--- CALCULADORA FODA/CALCULADORA.Designer.cs
+++ CALCULADORA FODA/CALCULADORA.Designer.cs
@@ -28,12 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CALCULADORA";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            visor = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(189, 109);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 33);
+            button1.TabIndex = 0;
+            button1.Text = "C";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(95, 186);
+            button2.Name = "button2";
+            button2.Size = new Size(88, 33);
+            button2.TabIndex = 1;
+            button2.Text = "3";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(189, 148);
+            button3.Name = "button3";
+            button3.Size = new Size(88, 33);
+            button3.TabIndex = 2;
+            button3.Text = "2";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(95, 148);
+            button4.Name = "button4";
+            button4.Size = new Size(88, 33);
+            button4.TabIndex = 3;
+            button4.Text = "1";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(95, 303);
+            button5.Name = "button5";
+            button5.Size = new Size(88, 33);
+            button5.TabIndex = 4;
+            button5.Text = "9";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(189, 303);
+            button6.Name = "button6";
+            button6.Size = new Size(88, 33);
+            button6.TabIndex = 5;
+            button6.Text = "0";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(283, 265);
+            button7.Name = "button7";
+            button7.Size = new Size(88, 71);
+            button7.TabIndex = 6;
+            button7.Text = "=";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(283, 226);
+            button8.Name = "button8";
+            button8.Size = new Size(88, 33);
+            button8.TabIndex = 7;
+            button8.Text = "+";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(283, 187);
+            button9.Name = "button9";
+            button9.Size = new Size(88, 33);
+            button9.TabIndex = 8;
+            button9.Text = "−";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(283, 148);
+            button10.Name = "button10";
+            button10.Size = new Size(88, 33);
+            button10.TabIndex = 9;
+            button10.Text = "×";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(283, 109);
+            button11.Name = "button11";
+            button11.Size = new Size(88, 33);
+            button11.TabIndex = 10;
+            button11.Text = "÷";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(95, 109);
+            button13.Name = "button13";
+            button13.Size = new Size(88, 33);
+            button13.TabIndex = 12;
+            button13.Text = "CE";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(189, 264);
+            button14.Name = "button14";
+            button14.Size = new Size(88, 33);
+            button14.TabIndex = 13;
+            button14.Text = "8";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(189, 225);
+            button15.Name = "button15";
+            button15.Size = new Size(88, 33);
+            button15.TabIndex = 14;
+            button15.Text = "6";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(95, 225);
+            button16.Name = "button16";
+            button16.Size = new Size(88, 33);
+            button16.TabIndex = 15;
+            button16.Text = "5";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(189, 186);
+            button17.Name = "button17";
+            button17.Size = new Size(88, 33);
+            button17.TabIndex = 16;
+            button17.Text = "4";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += button17_Click;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(95, 264);
+            button18.Name = "button18";
+            button18.Size = new Size(88, 33);
+            button18.TabIndex = 17;
+            button18.Text = "7";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // visor
+            // 
+            visor.Location = new Point(95, 46);
+            visor.Multiline = true;
+            visor.Name = "visor";
+            visor.Size = new Size(276, 43);
+            visor.TabIndex = 18;
+            // 
+            // CALCULADORA
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(visor);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "CALCULADORA";
+            Text = "CALCULADORA";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private TextBox visor;
     }
 }
--- CALCULADORA FODA/CALCULADORA.cs
+++ CALCULADORA FODA/CALCULADORA.cs
@@ -12,9 +12,45 @@ namespace CALCULADORA_FODA
 {
     public partial class CALCULADORA : Form
     {
+
+
+        float valor1 = 0;
+        string operacao = "";
+
         public CALCULADORA()
         {
             InitializeComponent();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            valor1 = 0;
+            visor.Text = visor.Text + "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "1";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "2";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "3";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "4";
         }
     }
 }
